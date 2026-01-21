5ae49e3a66b99b39ffda902ea4a5eca467db4698
Tannu Kumari <121978242+tannu371@users.noreply.github.com>
2026-01-21 05:32:20 +0530

    update

--- config/mailer.js
+++ config/mailer.js
@@ -4,6 +4,9 @@ env.config();
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // Use SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
